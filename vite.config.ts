060c0e645fd44791008d9c454fd930a6a752fe86
Legio46 <legiox46@gmail.com>
2025-09-10 16:52:16 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -20,3 +20,13 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
+
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+base: './',              // ensure relative paths, e.g. in index.html
+build: {
+    outDir: 'dist',      // gets deployed to github pages
+    assetsDir: 'assets', // static assets (images, js, css, etc.)
+},
+});
